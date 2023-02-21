cf6db93cbaeb89dc8ad6562dede606449118258c
GauravJoshiJi <125949334+GauravJoshiJi@users.noreply.github.com>
2023-02-21 11:33:24 +0530

    stopWatch

---
+++ script.js
@@ -0,0 +1,83 @@
+// initilzed all values with zero
+
+var hr = 0;
+var min = 0;
+var sec = 0;
+var count = 0;
+
+var timer = false;
+
+// start button logic
+function start() {
+  timer = true;
+  stopWatch();
+}
+
+// stop button logic
+function stop() {
+  timer = false;
+}
+
+// reset button logic
+function reset() {
+  timer = false;
+
+  hr = 0;
+  min = 0;
+  sec = 0;
+  count = 0;
+
+  document.getElementById("hr").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+  document.getElementById("count").innerHTML = "00";
+}
+
+// Main logic for Stop watch
+function stopWatch() {
+  if (timer == true) {
+    count = count + 1;
+
+    if (count == 100) {
+      sec = sec + 1;
+      count = 0;
+    }
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+
+    // intiger to string converson logic
+
+    var hrString = hr;
+    var minString = min;
+    var secString = sec;
+    var countString = count;
+
+    if (hr < 10) {
+      hrString = "0" + hrString;
+    }
+    if (min < 10) {
+      minString = "0" + minString;
+    }
+    if (sec < 10) {
+      secString = "0" + secString;
+    }
+    if (count < 10) {
+      countString = "0" + countString;
+    }
+
+    document.getElementById("hr").innerHTML = hrString;
+    document.getElementById("min").innerHTML = minString;
+    document.getElementById("sec").innerHTML = secString;
+    document.getElementById("count").innerHTML = countString;
+    setTimeout("stopWatch()", 10);
+  }
+}
